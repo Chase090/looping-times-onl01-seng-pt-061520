26c09f451143ccd1c887c470bf26629e9dfac67a
Carl Michael Oyam <carloyam21@icloud.com>
2020-06-21 02:58:40 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,7 @@
 def using_times
-	3.times do 
-  puts "Hiding 10 stolen jewels."
-  jewels_in_bag = jewels_in_bag - 10#your code here
+	7.times do 
+    puts "Hiding 10 stolen jewels."
+  end
 end
 
 
